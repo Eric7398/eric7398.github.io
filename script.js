33e5ee08402bb346f16b8f3c56525f1075e0041e
Eric Wang <72224088+Eric7398@users.noreply.github.com>
2022-01-07 18:58:14 -0800

    Added Carousel and About me

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let isModelOpen = false;
+let isAboutOpen = false;
 let contrastToggle = false;
+
+var slideIndex = 1;
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
@@ -42,4 +45,41 @@ function toggleModel() {
   }
   isModelOpen = true;
   document.body.classList += " model--open"
+}
+function toggleAbout() {
+  if (isAboutOpen) {
+    isAboutOpen = false;
+    return document.body.classList.remove("about--open")
+  }
+  isAboutOpen = true;
+  document.body.classList += " about--open"
+  showSlides(slideIndex);
+}
+
+
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
